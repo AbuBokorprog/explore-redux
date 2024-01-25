d847756d9746d68f2c87f39773c5db2e91258280
AbuBokorprog <abubokor1066@gmail.com>
2024-01-25 17:41:59 +0600

    add payload

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../redux/features/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByValue,
+} from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
   const { value } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   return (
     <div className="flex justify-evenly">
+      {/* increment by 5 */}
+      <button
+        onClick={() => dispatch(() => dispatch(incrementByValue(5)))}
+        className="px-5 py-2 bg-green-400 rounded-md"
+      >
+        Increment by 5
+      </button>
       <button
         onClick={() => dispatch(increment())}
         className="px-5 py-2 bg-green-400 rounded-md"
