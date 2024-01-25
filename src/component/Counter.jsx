cf1f9303d0195d903756b4b7abe957d4cf5f167f
AbuBokorprog <abubokor1066@gmail.com>
2024-01-25 17:30:11 +0600

    Counter  reduce and action

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
-  return <div></div>;
+  const { value } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div className="flex justify-evenly">
+      <button
+        onClick={() => dispatch(increment())}
+        className="px-5 py-2 bg-green-400 rounded-md"
+      >
+        Increment
+      </button>
+      <h1>{value}</h1>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="px-5 py-2 bg-red-400 rounded-md"
+      >
+        Decrement
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Counter from "../component/Counter";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Counter />
     </div>
   );
 };
